397fcb8351bcf7f613297204363cb8fb89ed2dab
provinzkraut <provinzkraut@posteo.de>
2025-05-04 14:45:16 +0200

    Formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 import os
 
-import pytest
 import django
-
 
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "tests.some_app.settings"
@@ -11,4 +9,3 @@ django.setup()
 
 # @pytest.fixture(scope="session", autouse=True)
 # def django_setup(monkeypatch) -> None:
-
